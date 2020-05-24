1765e1b6fd7a4ae418ad23360cca2f8f3c24c48a
Ivan Soriano <ivansoriarab@hotmail.com>
2020-05-24 13:54:31 +0200

    Centrar Spinner

--- view/dialog/js/loadingDialog.js
+++ view/dialog/js/loadingDialog.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     let dialog = $("dialog");
     let dialogContent = dialog.find("div.mdl-dialog__content");
 
-    let spinner = $("<div class='mdl-spinner mdl-js-spinner is-active'></div>");
+    let spinner = $("<center><div class='mdl-spinner mdl-js-spinner is-active'></div></center>");
     componentHandler.upgradeElement($(spinner)[0]);
     dialogContent.append($(spinner)[0]);
 });
