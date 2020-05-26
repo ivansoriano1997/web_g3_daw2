c78164cdd007f9ab0a7691ee38c8c8023e9b6136
Arnau Carrasco <arnacacu@gmail.com>
2020-05-26 20:18:35 +0200

    validaciones echas falta poner modales en el login

--- view/js/signupValidation.js
+++ view/js/signupValidation.js
@@ -53,17 +53,17 @@ $("input[name='drivingLicenseNumber']").keyup(function() {
        })
         //test carnet
         if (user=="" || user==undefined || user=="NULL"){            
-           $('#dialag').html(dialag('Error!', "Si us plau, posa un camp de correu electrònic vàlid!"));
+           $('#dialag').html(dialag('Error!', "Si us plau, posa un camp de correu electrònic vàlid!", 1));
            $('.errosSignup').show();
            $('.errosSignup').css('z-index', '1');
         }else{success++;}
         if (pass=="" || pass==undefined || pass=="NULL") {
-            $('#dialag').html(dialag('Error!', "Si us plau, posa un camp de contrasenya vàlid!"));
+            $('#dialag').html(dialag('Error!', "Si us plau, posa un camp de contrasenya vàlid!", 1));
             $('.errosSignup').show();
             $('.errosSignup').css('z-index', '1');           
         }else{success++;}
         if(model=="" || model==undefined || model=="NULL"){
-            $('#dialag').html(dialag('Error!', "Si us plau, selecciona un marca i model de cotxe vàlid!"));
+            $('#dialag').html(dialag('Error!', "Si us plau, selecciona un marca i model de cotxe vàlid!", 1));
             $('.errosSignup').show();
             $('.errosSignup').css('z-index', '1');   
         }else{success++;}
@@ -83,8 +83,11 @@ $("input[name='drivingLicenseNumber']").keyup(function() {
                 type: 'post',
                 success : function (success) {
                     if (success) {
-                        alert('Usuari enregistrat correctament!');
-                        window.location = "/route";
+                   
+                        $('#dialag').html(dialag('Ara ja formes part del g3d2!', "Usuari enregistrat correctament!", 2));
+                        $('.errosSignup').show();
+                        $('.errosSignup').css('z-index', '1');   
+          
                     }
                
                 }
@@ -101,9 +104,9 @@ $("input[name='drivingLicenseNumber']").keyup(function() {
 });
 
 
-function dialag(titol, mistage) {
-
- return  '<html>'+
+function dialag(titol, mistage, n) {
+if (n==1) {
+    return  '<html>'+
     '<head>'+
         '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dialog-polyfill/0.5.1/dialog-polyfill.min.css" integrity="sha256-qVjBFbunjrWQ4IikV5dkK1ziW9XKskisX1rUbCIJEhk=" crossorigin="anonymous" />'+
     '</head>'+
@@ -121,8 +124,32 @@ function dialag(titol, mistage) {
  '<script src="https://cdnjs.cloudflare.com/ajax/libs/dialog-polyfill/0.5.1/dialog-polyfill.min.js" integrity="sha256-B4Jb9j0PE2EzFJ6NaPkoJXDcQtQYR0Zd3DJKeYLifbg=" crossorigin="anonymous"></script>'+
  '</body>'+
  '</html>';
+}else if(n==2){
+    return  '<html>'+
+    '<head>'+
+        '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dialog-polyfill/0.5.1/dialog-polyfill.min.css" integrity="sha256-qVjBFbunjrWQ4IikV5dkK1ziW9XKskisX1rUbCIJEhk=" crossorigin="anonymous" />'+
+    '</head>'+
+    '<body>'+
+ 
+ '<dialog class="mdl-dialog errosSignup">'+
+
+ '<h4 class="mdl-dialog__title">'+titol+'</h4>'+
+ '<div class="mdl-dialog__content">'+
+ '<p>'+mistage+'</p>'+
+ '<div class="mdl-dialog__actions">'+
+ '<button type="button" class="mdl-button" onclick="urlroute()">Acceptar</button>'+
+ '</div>'+
+ '</dialog>'+    
+ '<script src="https://cdnjs.cloudflare.com/ajax/libs/dialog-polyfill/0.5.1/dialog-polyfill.min.js" integrity="sha256-B4Jb9j0PE2EzFJ6NaPkoJXDcQtQYR0Zd3DJKeYLifbg=" crossorigin="anonymous"></script>'+
+ '</body>'+
+ '</html>';
+}
+
 }
 function closedialog() {
     $("dialog").hide();
 }
 
+function urlroute(){
+    window.location = "/route";
+}
