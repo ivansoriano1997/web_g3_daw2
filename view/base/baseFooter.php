aeec33a5cd1251f023c907b35ce2368c95c4fd46
Ivan Soriano <ivansoriarab@hotmail.com>
2020-05-29 15:18:08 +0200

    Eliminem parts del footer sense definir

--- view/base/baseFooter.php
+++ view/base/baseFooter.php
@@ -14,48 +14,28 @@
     <body>
       <footer class="mdl-mega-footer">
         <div class="mdl-mega-footer__middle-section">
-        <div class="mdl-mega-footer__drop-down-section">
-          <input class="mdl-mega-footer__heading-checkbox" type="checkbox" checked>
-          <h1 class="mdl-mega-footer__heading">Features</h1>
-          <ul class="mdl-mega-footer__link-list">
-            <li><a href="#">About</a></li>
-            <li><a href="#">Terms</a></li>
-            <li><a href="#">Partners</a></li>
-            <li><a href="#">Updates</a></li>
-          </ul>
-        </div>
+          <div class="mdl-mega-footer__drop-down-section">
+            <input class="mdl-mega-footer__heading-checkbox" type="checkbox" checked>
+            <h1 class="mdl-mega-footer__heading">Persones implicades</h1>
+            <ul class="mdl-mega-footer__link-list">
+              <li><a href="https://www.linkedin.com/in/ivansorianoarabia/">Ivan</a></li>
+              <li><a href="https://www.linkedin.com/in/arnau-carrasco/">Arnau</a></li>
 
-        <div class="mdl-mega-footer__drop-down-section">
-          <input class="mdl-mega-footer__heading-checkbox" type="checkbox" checked>
-          <h1 class="mdl-mega-footer__heading">Persones implicades</h1>
-          <ul class="mdl-mega-footer__link-list">
-            <li><a href="https://www.linkedin.com/in/ivansorianoarabia/">Ivan</a></li>
-            <li><a href="https://www.linkedin.com/in/arnau-carrasco/">Arnau</a></li>
+            </ul>
+          </div>
 
-          </ul>
-        </div>
-
-        <div class="mdl-mega-footer__drop-down-section">
-          <input class="mdl-mega-footer__heading-checkbox" type="checkbox" checked>
-          <h1 class="mdl-mega-footer__heading">Tecnologies</h1>
-          <ul class="mdl-mega-footer__link-list">
-            <li><a href="freepik.es">Freepik</a></li>
-            <li><a href="getmdl.io">Material Design Lite</a></li>
-            <li><a href="code.visualstudio.com">Visual Studio Code</a></li>
-            <li><a href="atom.io">Atom</a></li>
-            <li><a href="github.com">GitHub</a></li>
-          </ul>
-        </div>
-
-        <div class="mdl-mega-footer__drop-down-section">
-          <input class="mdl-mega-footer__heading-checkbox" type="checkbox" checked>
-          <h1 class="mdl-mega-footer__heading">FAQ</h1>
-          <ul class="mdl-mega-footer__link-list">
-            <li><a href="#">Questions</a></li>
-            <li><a href="#">Answers</a></li>
-            <li><a href="#">Contact us</a></li>
-          </ul>
-        </div>
+          <div class="mdl-mega-footer__drop-down-section">
+            <input class="mdl-mega-footer__heading-checkbox" type="checkbox" checked>
+            <h1 class="mdl-mega-footer__heading">Tecnologies</h1>
+            <ul class="mdl-mega-footer__link-list">
+              <li><a href="freepik.es">PHP + MVC</a></li>
+              <li><a href="getmdl.io">Material Design Lite</a></li>
+              <li><a href="https://www.plesk.com">Plesk</a></li>
+              <li><a href="github.com">GitHub</a></li>
+              <li><a href="code.visualstudio.com">Visual Studio Code</a></li>
+              <li><a href="atom.io">Atom</a></li>
+            </ul>
+          </div>
       </div>
     </footer>
   </body>
